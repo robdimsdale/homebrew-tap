d5e23a25416daa434b488d21eeeb8e6e118e470c
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-09-21 07:08:50 -0700

    Install wundergo from source instead of copying binary.
    - Start from wundergo 0.7.0

--- wundergo.rb
+++ wundergo.rb
@@ -1,15 +1,26 @@
+require "language/go"
+
 class Wundergo < Formula
   desc "golang wunderlist client"
   homepage "https://github.com/robdimsdale/wundergo"
-  version "0.8.0"
-  url "https://github.com/robdimsdale/wundergo/releases/download/v#{version}/wl-darwin-amd64-#{version}"
-  sha256 "07d9f1131f9d626ed9534e14cfe82b7796b117dcdd68347bb5686e3e250c613b"
+  version "0.7.0"
+  url "https://github.com/robdimsdale/wundergo/archive/v#{version}.tar.gz"
+  sha256 "15e659b9e276ea9683ed1465f782c9e239c9e40578ba3039c0e1b5dca232c60e"
 
-  depends_on :arch => :x86_64
+  depends_on "go" => :build
+
+  go_resource "gopkg.in/yaml.v2" do
+      url "https://gopkg.in/yaml.v2.git", :revision => "7ad95dd0798a40da1ccdff6dff35fd177b5edf40"
+  end
 
   def install
-    bin.install "wl-darwin-amd64-#{version}"
-    mv bin/"wl-darwin-amd64-#{version}", bin/"wl"
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/robdimsdale/"
+    ln_sf buildpath, buildpath/"src/github.com/robdimsdale/wundergo"
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    system "go", "build", "-o", "wl", "-ldflags", "-X main.version=#{version}", "./cmd/wl/"
+    bin.install "wl"
   end
 
   test do
