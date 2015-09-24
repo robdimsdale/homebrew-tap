f5cbfa2a078baecdedfdaa4d6b55f688be189903
Concourse CI <ci@wundergo.io>
2015-09-24 04:32:53 +0000

    Update wundergo to version 0.11.0

--- wundergo.rb
+++ wundergo.rb
@@ -3,14 +3,14 @@ require "language/go"
 class Wundergo < Formula
   desc "golang wunderlist client"
   homepage "https://github.com/robdimsdale/wundergo"
-  version "0.10.1"
+  version "0.11.0"
   url "https://github.com/robdimsdale/wundergo/archive/v#{version}.tar.gz"
-  sha256 "6b086af4ae029e4fbd65453d0335d171732e5f0ec0ddc43fc2b6643b4495f354"
+  sha256 "a1342fdf3a50abba8db64a7d8c5941db0f88c50fcba155fe06843d598f64cb08"
 
   depends_on "go" => :build
 
-  go_resource "gopkg.in/yaml.v2" do
-      url "https://gopkg.in/yaml.v2.git", :revision => "7ad95dd0798a40da1ccdff6dff35fd177b5edf40"
+  go_resource "github.com/tools/godep" do
+    url "https://github.com/tools/godep.git", :revision => "2c6ee5e071cf01a41359e33502b7fe79a5801b9e"
   end
 
   def install
@@ -19,7 +19,11 @@ class Wundergo < Formula
     ln_sf buildpath, buildpath/"src/github.com/robdimsdale/wundergo"
     Language::Go.stage_deps resources, buildpath/"src"
 
-    system "go", "build", "-o", "wl", "-ldflags", "-X main.version=#{version}", "./cmd/wl/"
+    cd "src/github.com/tools/godep" do
+      system "go", "install"
+    end
+
+    system "./bin/godep", "go", "build", "-o", "wl", "-ldflags", "-X main.version=#{version}", "./cmd/wl/"
     bin.install "wl"
   end
 
