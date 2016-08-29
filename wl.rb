394e3d9bce9b59e7c4432d877b2298ec49846a55
Concourse CI <ci@robdimsdale.com>
2016-08-29 04:28:55 +0000

    Update wl to version 1.2.4

--- wl.rb
+++ wl.rb
@@ -1,33 +1,20 @@
-require "language/go"
+require 'formula'
 
 class Wl < Formula
   desc "golang wunderlist client"
   homepage "https://github.com/robdimsdale/wl"
-  version "1.2.2"
+  version "1.2.4"
   url "https://github.com/robdimsdale/wl/archive/v#{version}.tar.gz"
-  sha256 "43b0297b3e67096d837cfbf0ee3538d102e43ab5780658b551d7f7e2a3dc347c"
+  sha256 "171683552d044f0fe343aadb64467e07c1d086be78b33715a26dc1ecbf9be8c5"
 
-  depends_on "go" => :build
-
-  go_resource "github.com/tools/godep" do
-    url "https://github.com/tools/godep.git", :revision => "2c6ee5e071cf01a41359e33502b7fe79a5801b9e"
-  end
+  depends_on :arch => :x86_64
 
   def install
-    ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/robdimsdale/"
-    ln_sf buildpath, buildpath/"src/github.com/robdimsdale/wl"
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    cd "src/github.com/tools/godep" do
-      system "go", "install"
-    end
-
-    system "./bin/godep", "go", "build", "-o", "wl", "-ldflags", "-X main.version=#{version}", "./cmd/wl/"
+    mv "wl-darwin-amd64-#{version}", "wl"
     bin.install "wl"
   end
 
   test do
-    system "#{bin}/wl", "--version"
+    system "#{bin}/wl"
   end
 end
