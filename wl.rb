6e2ee3f4d0e2c429da0f8bed496c2677a0416938
Concourse CI <ci@robdimsdale.com>
2016-08-29 04:32:56 +0000

    Update wl to version 1.2.4

--- wl.rb
+++ wl.rb
@@ -4,7 +4,7 @@ class Wl < Formula
   desc "golang wunderlist client"
   homepage "https://github.com/robdimsdale/wl"
   version "1.2.4"
-  url "https://github.com/robdimsdale/wl/archive/v#{version}.tar.gz"
+  url "https://github.com/robdimsdale/wl/releases/download/v#{version}/wl-darwin-amd64-#{version}"
   sha256 "171683552d044f0fe343aadb64467e07c1d086be78b33715a26dc1ecbf9be8c5"
 
   depends_on :arch => :x86_64
