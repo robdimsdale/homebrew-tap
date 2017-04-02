104b6460b2bde9b758aece06b117d00a7db2a695
Concourse CI <ci@robdimsdale.com>
2017-04-02 23:07:14 +0000

    Update wl-cli to version 2.0.0-rc.2

--- wl-cli.rb
+++ wl-cli.rb
@@ -4,7 +4,7 @@ class WlCli < Formula
   desc "Unofficial Wunderlist CLI"
   homepage "https://github.com/robdimsdale/wl-cli"
   version "2.0.0-rc.2"
-  url "https://github.com/robdimsdale/wl-cli/releases/download/v#{version}/wl-darwin-amd64-#{version}"
+  url "https://github.com/robdimsdale/wl-cli/releases/download/v#{version}/wl-#{version}-darwin-amd64"
   sha256 "0cbe645f098fc5cd6466fe7f94e82dde35408b53d153c4b43e54059b5ab0a3a6"
 
   depends_on :arch => :x86_64
